bd177c39a3fcc39211efd85617ef821219c4f8ee
FTLDev <vbifmkvl@yandex.ru>
2021-04-11 00:04:04 +0300

    4.0

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
+// import { map } from 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -9,10 +12,12 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(
-      map(response => response.json())
-    );
+  getAlbum(id: number): Observable<Album> {
+    // return this._http.get(this._albumUrl).pipe(
+    //   map(response => response.json())
+    // );
+    return this._http.get(this._albumUrl).map(
+          response => <Album>response.json());
   }
 
 }
